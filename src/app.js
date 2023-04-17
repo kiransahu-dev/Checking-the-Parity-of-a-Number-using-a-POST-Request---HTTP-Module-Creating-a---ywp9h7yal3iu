a8a79029aaa07cc435613e69a2bf85266121d2ca
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-04-17 09:47:32 +0530

    code written for even odd

--- src/app.js
+++ src/app.js
@@ -10,13 +10,24 @@ const server = http.createServer((req, res) => {
       chunks.push(str);
       const obj = JSON.parse(chunks)
       const value = obj.num1;
-    
-     // Write the code here to check if the number is odd or even
 
-   });
+      // Write the code here to check if the number is odd or even
+      if (value.num1 == undefined) {
+        res.writeHead(400, { 'Content- Type': 'text / plain' });
+        res.end("Bad request")
+      }
+      else if (value.num1 % 2 == 0) {
+        res.writeHead(200, { 'Content- Type': 'text / plain' });
+        res.end("The number is even");
+      }
+      else {
+        res.writeHead(404, { 'Content- Type': 'text / plain' });
+        res.end("The number is odd");
+      }
+    });
   }
 
-  
+
 });
 
 
