7ac3d3277d41663a2992688e254e3531398a46f7
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-04-19 10:36:42 +0530

    commited for final testcases

--- src/app.js
+++ src/app.js
@@ -12,17 +12,17 @@ const server = http.createServer((req, res) => {
       const value = obj.num1;
 
       // Write the code here to check if the number is odd or even
-      if (value == undefined) {
-        res.writeHead(400, { 'Content- Type': 'text / plain' });
-        res.end("Bad request")
-      }
-      else if (value % 2 == 0) {
+      if (value % 2 == 0) {
         res.writeHead(200, { 'Content- Type': 'text / plain' });
         res.end("The number is even");
       }
-      else {
+      else if(value %2 != 0) {
         res.writeHead(404, { 'Content- Type': 'text / plain' });
         res.end("The number is odd");
+      }
+       else {
+        res.writeHead(400, { 'Content- Type': 'text / plain' });
+        res.end("Bad request")
       }
     });
   }
