17d78a0c87513eed0d492f7107bc7e34804dcd15
Kiran Sahu <61940769+kiransahu-dev@users.noreply.github.com>
2023-04-23 09:01:10 +0530

    updated as per the condition

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const server = http.createServer((req, res) => {
         res.end(`The number ${value} is odd`);
       }
       else{
-        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end(`Error message`);
       }
    });
